a31b729aeef528080a467f252e9a0957214a8b12
Ashish Dubey <adg1974sop@gmail.com>
2026-03-29 17:20:07 +0530

    Delete src directory

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api',
-  googleClientId: '858543807352-vumqdk5gj1g76h03oqos8nsumva1bjv5.apps.googleusercontent.com'
-};
