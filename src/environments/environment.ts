29ba4068b011e3fa5925ba4a962110c4fbea0d3d
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-03-29 17:15:47 +0530

    [Ashish Dubey] Add frontend files for QuantityMeasurementApp using Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5000/api',
+  googleClientId: '858543807352-vumqdk5gj1g76h03oqos8nsumva1bjv5.apps.googleusercontent.com'
+};
